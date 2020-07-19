4caff777e3ff1105bbfaf1c8bd670bd485c768b2
rohanmittal01 <rohanmittal01@gmail.com>
2020-07-20 02:31:04 +0530

    added Get tasks and task by id in practice-node

--- practice-node/src/index.js
+++ practice-node/src/index.js
@@ -34,7 +34,7 @@ app.get('/users/:id', (req,res) => {
         if(!user){
             return res.status(404).send()
         }
-        res.status(201).send(user)
+        res.status(200).send(user)
     }).catch((e) => {
         res.status(500).send()
     })
@@ -52,7 +52,26 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+app.get('/tasks', (req,res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
 
+app.get('/tasks/:id', (req,res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send()
+        }
+        res.status(200).send(task)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
 
 app.listen(port, () => {
     console.log('Server is up on port '+port)
